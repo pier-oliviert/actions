050ddd6cf2993ae4da279f4757fca21333f5c173
Pier-Olivier Thibault <23230+pier-oliviert@users.noreply.github.com>
2024-11-09 13:18:21 -0500

    Fix

--- src/changelogs/index.ts
+++ src/changelogs/index.ts
@@ -1,83 +1,12 @@
 import * as core from '@actions/core';
-import { parse, SemVer } from "semver";
-import { Octokit } from "@octokit/core";
 import { $ } from "bun";
 
-interface Range {
-  older?: Tag
-  newer?: Tag
-}
-
-interface Tag {
-  commit: string
-  version: SemVer
-}
-
 export async function execute() {
-  const authToken = core.getInput("auth_token")!
-  let envName = core.getInput("variable") || "VERSION"
   const changelogFile = core.getInput("changelog_file") || "CHANGELOG.md"
   const mainBranch = core.getInput("main_branch") || "main"
-  const repoPath = core.getInput("repo")!
-
-  const [owner, repo] = repoPath.split("/")
-
-  // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-  const octokit = new Octokit({ auth: authToken });
-
-  const variables = await octokit.request('GET /repos/{owner}/{repo}/actions/variables', {
-    owner: owner,
-    repo: repo,
-  })
-
-
-  let current = parse(variables.data.variables.find(v => v.name == envName)?.value)
-
-  const tags = await octokit.request('GET /repos/{owner}/{repo}/tags', {
-    owner: owner,
-    repo: repo,
-    per_page: 4,
-  })
-
-  console.log("Finding range for changelog")
-  let bounds: Range = {}
-
-  for (let t of tags.data) {
-    const version = parse(t.name)
-    if (!version) {
-      continue
-    }
-
-    if (!bounds.newer) {
-      bounds.newer = {
-        commit: t.commit.sha,
-        version: version,
-      }
-      continue
-    }
-
-    if (bounds.newer.version.compare(version) == 1) {
-      bounds.older = {
-        commit: t.commit.sha,
-        version: version,
-      }
-    }
-  }
-
-  if (current == null) {
-    console.log("No version present in repository variables, creating one.")
-    octokit.request('POST /repos/{owner}/{repo}/actions/variables', {
-      owner: owner,
-      repo: repo,
-      name: envName,
-      value: `v${bounds.newer?.version.toString()}`
-    })
-  }
-
-
-  if (!bounds.older) {
-    console.log("Can't create a changelog as there's no older tag, a changelog will be generated when the next tag is created")
-  }
+  const version = core.getInput("version")
+  const startHash = core.getInput("version_start_hash")!
+  const endHash = core.getInput("version_end_hash")
 
   const file = Bun.file(changelogFile);
   let content
@@ -86,7 +15,14 @@ export async function execute() {
     content = await file.text()
   }
 
-  const changelog = await $`git-cliff ${bounds.older?.commit}..${bounds.newer?.commit}`.text()
+  if (!endHash) {
+    console.log("Can't create a changelog as there's no older tag, a changelog will be generated when the next tag is created")
+    process.exit(0)
+  }
+
+  // end -> start as the end is the oldest hash and the newest is the commit that was tagged
+  // the new version
+  const changelog = await $`git-cliff ${endHash}..${startHash}`.text()
 
   const writer = file.writer()
   writer.write(changelog)
@@ -101,9 +37,7 @@ export async function execute() {
   // https://github.com/oven-sh/bun/issues/8745
   const emoji = "📌"
 
-  await $`git add -f ${changelogFile} && git commit -m "${emoji} Changelog for ${bounds.newer?.version}" && git push origin ${mainBranch}`
+  await $`git add -f ${changelogFile} && git commit -m "${emoji} Changelog for ${version}" && git push origin ${mainBranch}`
 
-  core.setOutput('previousVersion', bounds.older?.version);
-  core.setOutput('releaseHash', bounds.newer?.commit);
   core.setOutput('changelog', changelog)
 }
--- src/versions/index.ts
+++ src/versions/index.ts
@@ -1,6 +1,17 @@
 import * as core from '@actions/core';
-import { parse } from "semver";
+import { parse, SemVer } from "semver";
 import { Octokit } from "@octokit/core";
+
+interface Range {
+  older?: Tag
+  newer?: Tag
+}
+
+interface Tag {
+  name: string
+  commit: string
+  version: SemVer
+}
 
 export async function execute() {
   let envName = core.getInput("variable") || "VERSION"
@@ -18,21 +29,37 @@ export async function execute() {
   })
 
   console.log("Getting the most recent versionned tag")
-  let tag
-  let version
+  let bounds: Range = {}
 
   for (let t of tags.data) {
-    tag = t
-    version = parse(t.name)
+    const version = parse(t.name)
     if (!version) {
       continue
     }
-    break
+
+    if (!bounds.newer) {
+      bounds.newer = {
+        commit: t.commit.sha,
+        name: t.name,
+        version: version,
+      }
+      continue
+    }
+
+    if (bounds.newer.version.compare(version) == 1) {
+      bounds.older = {
+        commit: t.commit.sha,
+        name: t.name,
+        version: version,
+      }
+    }
   }
 
-  if (!version) {
+  if (!bounds.newer) {
     core.setFailed("No version found in the last 4 tags on this repo.");
   }
+
+  bounds.newer = bounds.newer!
 
   const variables = await octokit.request('GET /repos/{owner}/{repo}/actions/variables', {
     owner: owner,
@@ -49,28 +76,27 @@ export async function execute() {
       owner: owner,
       repo: repo,
       name: envName,
-      value: `v${version?.toString()}`
+      value: `v${bounds.newer.version.toString()}`
     })
-    core.setOutput('version', version)
-    core.setOutput('tag', tag?.name)
+
+    core.setOutput('version', bounds.newer.version.toString())
+    core.setOutput('tag', bounds.newer.name)
+    core.setOutput('version_start_hash', bounds.newer.commit)
     process.exit(0)
   }
 
-  if (version?.compare(parse(variable?.value) || "") == 1) {
-    console.log(`Updating version variable to ${version?.toString()}`)
+  console.log(`Updating version variable to ${bounds.newer.version.toString()}`)
 
-    await octokit.request('PATCH /repos/{owner}/{repo}/actions/variables/{name}', {
-      owner: owner,
-      repo: repo,
-      name: envName,
-      value: `v${version?.toString()}`
-    })
+  await octokit.request('PATCH /repos/{owner}/{repo}/actions/variables/{name}', {
+    owner: owner,
+    repo: repo,
+    name: envName,
+    value: `v${bounds.newer.version.toString()}`
+  })
 
-    core.setOutput('version', version.toString())
-    core.setOutput('tag', tag?.name)
-    process.exit(0)
-  }
-
-  core.setFailed(`Version is not newer than what is already set version variable to ${variable?.value}`)
-
+  core.setOutput('version', bounds.newer.version.toString())
+  core.setOutput('tag', bounds.newer.name)
+  core.setOutput('version_start_hash', bounds.newer.commit)
+  core.setOutput('version_end_hash', bounds.older?.commit)
+  process.exit(0)
 }
